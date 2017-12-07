9a2a020d4a70f614a77efbe24ecd98b664212946
taichikoumoto <sbskomoto@gmail.com>
2017-12-07 11:46:25 +0900

    -, ., 0割り算をバリデーション

--- app/src/main/java/taichi/calcapp/MainActivity.java
+++ app/src/main/java/taichi/calcapp/MainActivity.java
@@ -8,6 +8,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.content.Intent;
 import android.util.Log;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -41,6 +45,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String editText1Str = editText1.getText().toString();
         String editText2Str = editText2.getText().toString();
         if (TextUtils.isEmpty(editText1Str) || TextUtils.isEmpty(editText2Str)) { return; }
+
+        List<String> invalidList = new ArrayList<>(Arrays.asList("-", "."));
+        if (invalidList.contains(editText1Str) || invalidList.contains(editText2Str)) {
+            Toast.makeText(this, "不正な値です", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (Integer.valueOf(editText2Str) == 0) {
+            Toast.makeText(this, "0で割り算はできません", Toast.LENGTH_SHORT).show();
+            return;
+        }
+//        API Level 24以上
+//        if (Arrays.asList(editText1Str, editText2Str).stream().anyMatch(str -> str == "+" || str == "-")) {
+//            Toast.makeText(this, "不正な値です", Toast.LENGTH_SHORT).show();
+//        }
         Float val1 = Float.valueOf(editText1Str);
         Float val2 = Float.valueOf(editText2Str);
 
