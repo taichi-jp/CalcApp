a15ba5f0d17773df1c1b3631c8d1daef3e20636b
taichikoumoto <sbskomoto@gmail.com>
2017-12-08 10:56:09 +0900

    修正

--- app/src/main/java/taichi/calcapp/MainActivity.java
+++ app/src/main/java/taichi/calcapp/MainActivity.java
@@ -2,21 +2,20 @@ package taichi.calcapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.content.Intent;
 import android.util.Log;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText editText1;
     EditText editText2;
+
+    Float val1;
+    Float val2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,23 +43,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         String editText1Str = editText1.getText().toString();
         String editText2Str = editText2.getText().toString();
-        if (TextUtils.isEmpty(editText1Str) || TextUtils.isEmpty(editText2Str)) { return; }
 
-        List<String> invalidList = new ArrayList<>(Arrays.asList("-", "."));
-        if (invalidList.contains(editText1Str) || invalidList.contains(editText2Str)) {
+        try {
+            val1 = Float.valueOf(editText1Str);
+            val2 = Float.valueOf(editText2Str);
+        } catch (NumberFormatException e) {
             Toast.makeText(this, "不正な値です", Toast.LENGTH_SHORT).show();
             return;
         }
-        if (Integer.valueOf(editText2Str) == 0) {
-            Toast.makeText(this, "0で割り算はできません", Toast.LENGTH_SHORT).show();
-            return;
-        }
-//        API Level 24以上
-//        if (Arrays.asList(editText1Str, editText2Str).stream().anyMatch(str -> str == "+" || str == "-")) {
-//            Toast.makeText(this, "不正な値です", Toast.LENGTH_SHORT).show();
-//        }
-        Float val1 = Float.valueOf(editText1Str);
-        Float val2 = Float.valueOf(editText2Str);
 
         Intent intent = new Intent(this, ResultActivity.class);
         if (v.getId() == R.id.buttonPlus) {
@@ -70,6 +60,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else if (v.getId() == R.id.buttonTimes) {
             intent.putExtra("VALUE", val1 * val2);
         } else if (v.getId() == R.id.buttonDiv) {
+            if (val2 == 0.0) {
+                Toast.makeText(this, "0で割り算はできません", Toast.LENGTH_SHORT).show();
+                return;
+            }
             intent.putExtra("VALUE", val1 / val2);
         } else {
             Log.d("UI_PARTS", "エラー！");
